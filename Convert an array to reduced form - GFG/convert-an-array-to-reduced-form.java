50b4a4213bc65ebfdcd8da3c8269f5b02dc88cf7
Khushbu <74493334+Khushbu1309@users.noreply.github.com>
2023-01-24 01:12:58 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().convert(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void convert(int[] arr, int n) {
+        //Code here
+        int[] temp=new int[n];
+        for(int i=0;i<arr.length;i++){
+            temp[i]=arr[i];
+        }
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<temp.length;i++){
+            hm.put(temp[i],i);
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=hm.get(arr[i]);
+        }
+        
+    }
+}
