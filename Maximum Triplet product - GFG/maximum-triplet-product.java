8dec95056e7dcd26da669a4c309ab7f009fcf2d4
Khushbu <74493334+Khushbu1309@users.noreply.github.com>
2023-03-10 20:54:22 +0530

    Added solution - LeetHub

---
+++ Maximum Triplet product - GFG/maximum-triplet-product.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    Long maxTripletProduct(Long arr[], int n)
+    {
+        // long ans=Integer.MIN_VALUE;
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         for(int k=j+1;k<arr.length;k++){
+        //             ans=Math.max((arr[i]*arr[j]*arr[k]),ans);
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        Arrays.sort(arr);
+
+        return Math.max(arr[0] * arr[1] * arr [n - 1], arr[n - 3] * arr[n - 2] * arr[n - 1]);
+        
+        
+        
+        
+        
+    }
+}
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    
+		    Long[] arr = new Long[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Long.parseLong(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    Long res = obj.maxTripletProduct(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+// } Driver Code Ends
