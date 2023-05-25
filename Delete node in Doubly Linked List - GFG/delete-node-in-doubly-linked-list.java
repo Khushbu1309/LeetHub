44b3c3198db661bbec5881292fdd6a38c3d5055f
Khushbu <74493334+Khushbu1309@users.noreply.github.com>
2023-05-26 02:25:17 +0530

    Added solution - LeetHub

---
+++ Delete node in Doubly Linked List - GFG/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int d)
+	{
+		data = d;
+		next = prev = null;
+	}
+}
+class Delete_Node_From_DLL
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node)
+	{
+		if(head ==  null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail.next.prev = tail;
+			tail = tail.next;
+		}
+	}
+	
+	void printList(Node head)
+	{	Node temp = head;
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp =  temp.next;
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			Delete_Node_From_DLL list = new Delete_Node_From_DLL();
+			
+			int a1 = sc.nextInt();
+			Node head = new Node(a1);
+			list.addToTheLast(head);
+			
+			for(int i=1;i<n;i++)
+			{
+				int a = sc.nextInt();
+				list.addToTheLast(new Node(a));
+			}
+			a1 = sc.nextInt();
+			Solution g = new Solution();
+			//System.out.println(list.temp.data);
+			Node ptr = g.deleteNode(list.head, a1);
+			list.printList(ptr);
+			t--;
+		}
+	}
+}
+
+
+                               
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Structure of linkedlist node
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int d)
+	{
+		data = d;
+		next = prev = null;
+	}
+}
+*/
+class Solution
+{
+    // function returns the head of the linkedlist
+    Node deleteNode(Node head,int x)
+    {
+	   Node h=head;
+	   if(x==1){
+	       head=head.next;
+	       return head;
+	   }
+	   int i=1;
+	   while(h!=null){
+	       if(i==x-1){
+	           if(h.next.next==null){
+	               h.next=null;
+	               
+	           }
+	           else{
+	               Node t=h.next;
+    	           h.next=h.next.next;
+    	           
+    	           h.next.prev=h;
+	               
+	           }
+	          
+	       
+	           break;
+	       }
+	       h=h.next;
+	       i++;
+	   }
+	   return head;
+    }
+}
