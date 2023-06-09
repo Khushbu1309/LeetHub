182709fb5eb3001109340df7c1941495575590d7
Khushbu <74493334+Khushbu1309@users.noreply.github.com>
2023-06-09 23:20:26 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String S = br.readLine().trim();
+            Solution obj = new Solution();
+            List<String> ans = obj.find_permutation(S);
+            for( int i = 0; i < ans.size(); i++)
+            {
+                out.print(ans.get(i)+" ");
+            }
+            out.println();
+                        
+        }
+        out.close();
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public List<String> find_permutation(String S) {
+        // Code here
+        ArrayList<String> al=new ArrayList<>();
+        
+        permute(S,0,S.length()-1,al);
+        Collections.sort(al);
+        return al;
+    }
+    private void permute(String str, int l, int r,ArrayList<String> al)
+    {
+        if (l == r){
+            if(!al.contains(str))
+                al.add(str);
+        }
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r,al);
+                str = swap(str, l, i);
+            }
+        }
+    }
+     public String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+}
