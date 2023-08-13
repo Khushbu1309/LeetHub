f6753b8dfd816b25f4f12a9e4fff73ed87dbc539
Khushbu Bora <74493334+Khushbu1309@users.noreply.github.com>
2023-08-14 00:56:17 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.java
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.java
@@ -23,29 +23,29 @@ class GFG {
 //User function Template for Java
 class Solution {
     static int nthFibonacci(int n){
-        // int[] dp=new int[n+2];
+        int[] dp=new int[n+2];
         
          
-        //  dp[0]=0%1000000007;
-        //  dp[1]=1%1000000007;
+         dp[0]=0%1000000007;
+         dp[1]=1%1000000007;
          
-        //  for(int i=2;i<=n;i++){
-        //      dp[i]=dp[i-1]+dp[i-2]%1000000007;
-        //      System.out.println(dp[i]+" ");
-        //  }
-        // return dp[n]%1000000007;
+         for(int i=2;i<=n;i++){
+             dp[i]=dp[i-1]%1000000007+dp[i-2]%1000000007;
+             //System.out.println(dp[i]+" ");
+         }
+        return dp[n]%1000000007;
         
         
         
-        int a = 0, b = 1, c;
-        if (n == 0)
-            return a;
-        for (int i = 2; i <= n; i++) {
-            c = (a + b)%1000000007;
-            a = b%1000000007;
-            b = c%1000000007;
-        }
-        return b;
+        // int a = 0, b = 1, c;
+        // if (n == 0)
+        //     return a;
+        // for (int i = 2; i <= n; i++) {
+        //     c = (a + b)%1000000007;
+        //     a = b%1000000007;
+        //     b = c%1000000007;
+        // }
+        // return b;
         
     }
     // public static int f(int i,int[] dp){
